f6800a254c65bc72dfc8bb12db12544cb730e52a
Seaan Wong <seanwong24@live.com>
2023-03-07 09:48:50 +0000

    add fake waiting time for data provider

--- plugins/data-provider.plugin.js
+++ plugins/data-provider.plugin.js
@@ -1,8 +1,21 @@
 export default class extends HTMLElement {
+  notifyLoadingDelegate;
+
   obtainHeaderCallback = () => `Data Provider`;
   obtainDataProviderIdentifiersCallback = () => ["demo"];
 
-  queryDataCallback = async (_identifier, _dataSource, _queryObject) => [
-    1, 2, 3, 4, 5,
-  ];
+  queryDataCallback = async (_identifier, _dataSource, _queryObject) => {
+    const EndLoadingDelegate = this.notifyLoadingDelegate?.();
+    await this.timeKiller(2000);
+    EndLoadingDelegate?.();
+    return [1, 2, 3, 4, 5];
+  };
+
+  async timeKiller(timeout) {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, timeout)
+    );
+  }
 }
